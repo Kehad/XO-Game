0c7f80e8759cbaf7d8256a0227cf0d73b852f41c
KEHINDE ADIGUN <keahnney01@gmail.com>
2026-03-12 02:32:29 +0100

    any team to play first

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,12 @@ function App() {
 
   const resetGame = () => {
     setBoard(Array(9).fill(null));
-    if (gameMode === 'offline_multiplayer') {
+    setScores({ X: 0, O: 0, Draws: 0 });
+  
+  };
+  const newGame = () => {
+    setBoard(Array(9).fill(null));
+    if (gameMode === 'offline_multiplayer' || gameMode === 'online_multiplayer' || gameMode === 'offline_computer') {
       const nextStarter = startingPlayer === 'X' ? 'O' : 'X';
       setStartingPlayer(nextStarter);
       setXIsNext(nextStarter === 'X');
@@ -271,6 +276,8 @@ function App() {
         />
 
         {/* Controls */}
+        <div className="flex gap-4 flex-row">
+
         <button
           onClick={resetGame}
           className="mt-4 bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-400 hover:to-purple-500 text-white font-bold py-3 px-8 rounded-full shadow-lg hover:shadow-indigo-500/25 active:scale-95 transition-all duration-200 focus:outline-none focus:ring-4 focus:ring-indigo-500/50 w-full sm:w-auto text-lg flex items-center justify-center gap-2 group"
@@ -278,7 +285,16 @@ function App() {
           <svg className="w-5 h-5 group-hover:-rotate-180 transition-transform duration-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" /></svg>
           Restart Game
         </button>
+        <button
+          onClick={newGame}
+          className="mt-4 bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-400 hover:to-purple-500 text-white font-bold py-3 px-8 rounded-full shadow-lg hover:shadow-indigo-500/25 active:scale-95 transition-all duration-200 focus:outline-none focus:ring-4 focus:ring-indigo-500/50 w-full sm:w-auto text-lg flex items-center justify-center gap-2 group"
+        >
+          <svg className="w-5 h-5 group-hover:-rotate-180 transition-transform duration-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" /></svg>
+          New Game
+        </button>
       </div>
+        </div>
+
 
       <style>{`
         @keyframes popIn {
