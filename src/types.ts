6be68e31dfc4b33cf2f64474852fe0efb9c5e9b7
KEHINDE ADIGUN <keahnney01@gmail.com>
2026-03-14 23:53:39 +0100

    started working on the online multiplayer 9

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,4 @@ export type PlayerType = 'X' | 'O' | null;
 export type GameMode = 'offline_computer' | 'offline_multiplayer' | 'online_multiplayer' | null;
 export type Difficulty = 'easy' | 'medium' | 'hard' | null;
 
-export interface GameState {
-    board: PlayerType[];
-    winner:  | null;
-}
+
