d79645929fad9e93689aadb07915d493954d69a2
KEHINDE ADIGUN <keahnney01@gmail.com>
2026-03-30 18:44:49 +0100

    x0 differeny

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.mjmg4ug91vg"
+    "revision": "0.279ivr8keb"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
